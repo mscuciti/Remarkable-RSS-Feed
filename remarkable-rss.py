9ce2e9c2d2891ce86eb7c4101c2c73b4bdecff8f
matt <matthewcuciti@gmail.com>
2025-06-01 14:35:08 -0400

    several changes, including placing the feeds in a seperate file to be read, rather than making the user modify the code. a better scraping function, most of it borrowed from https://github.com/MrPike/RSS2Ebook as well as making the output a bit prettier.

--- remarkable-rss.py
+++ remarkable-rss.py
@@ -1,3 +1,6 @@
+from urllib.request import Request, urlopen
+import shutil
+import json
 import os
 import feedparser
 from datetime import datetime
@@ -8,6 +11,76 @@ from fpdf import FPDF
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.http import MediaFileUpload
+from goose3 import Goose
+import subprocess
+
+# Edit the following according to your preferences.
+MAX_RETRIES = 10  # Amount of Network-Error based request retries
+WD = os.path.dirname(os.path.realpath(__file__))  # Current working Directory
+HTML_DIR = "HTML_Out"  # Storage location for the extracted HTML source
+TEMP_FILE = 'temp.txt'  # A temporary storage file for downloaded text
+
+FEED_URLS = []
+EBOOK_NAMES = []
+
+f = open("feeds.txt", "r")
+d = json.loads(f.read())
+for i in d:
+    FEED_URLS.append(d[i])
+    EBOOK_NAMES.append(i)
+
+# Generate the file path for saving the RSS data
+date_str = datetime.now().strftime('%Y-%m-%d')
+file_path = f"RSS-Feed_{date_str}"
+EBOOK_NAME = f"{file_path}.pdf"
+
+def rss_to_ebook(my_url,my_name):
+    # Create HTML storage dir
+    if os.path.exists(HTML_DIR):
+        shutil.rmtree(HTML_DIR, ignore_errors=True)
+    os.mkdir(HTML_DIR)
+    d = feedparser.parse(my_url)
+
+    # Enumerate through our feed, store the 'clean' article text in our temporary file, before exporting it to HTML
+    for c, e in enumerate(d.entries):
+        url = e.link
+        print('Processing - {}'.format(url))
+        g = Goose()
+        retries = MAX_RETRIES
+        while retries > 0:
+            try:
+                req = Request(url)
+                req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0')
+                req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8')
+                req.add_header('Accept-Language', 'en-US,en;q=0.5')
+                webpage = urlopen(req).read()
+                g = Goose()
+                article = g.extract(raw_html=webpage)
+                g.close
+                break
+            except Exception as e:
+                print('Error establishing connection. Retrying...')
+                retries -= 1
+        if retries <= 0:
+            print('Failed to retreive article ({}). Moving on to next feed item.'.format(url))
+            retries = MAX_RETRIES
+        with open('temp.txt', 'w') as file:
+            file.write(article.cleaned_text)
+        of = HTML_DIR + '/s{}-{}.html'.format('{0:05d}'.format(c+1), url.split("/")[-1].split(".")[0])
+        subprocess.Popen('pandoc -i temp.txt -t html5 -o {}'.format(of), cwd=WD, shell=True).wait()
+        with open(of, 'r+') as f:
+            content = f.read()
+            f.seek(0, 0)
+            f.write('<h1>{}</h1>'.format(article.title).rstrip('\r\n') + '\n' + content)
+        # Feed title and Link are required as per the RSS2 spec ... we don't check if there's actually a value
+        with open(HTML_DIR + '/s00000.html', 'w') as file:
+            file.write('<!DOCTYPE html><html><head><meta name="author" content="{}" />'
+                       '<title>{}</title></head></html>'.format(d.feed.link, d.feed.title))
+    # Convert the directory of (Ordered HTML file's into a single EPub)
+    subprocess.Popen('pandoc --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt -s -i {}/*.html -t pdf -o {} --toc'.format(HTML_DIR, my_name),  cwd=WD, shell=True).wait()
+
+    # Cleanup
+    os.remove('temp.txt')
 
 # Load environment variables
 load_dotenv()
@@ -42,66 +115,8 @@ def upload_to_google_drive(file_path, folder_id=None):
     except Exception as e:
         print(f"Upload to Google Drive failed: {e}")
 
-
-def fetch_rss_feed(rss_url):
-    """Fetch and parse the RSS feed."""
-    return feedparser.parse(rss_url)
-
-def extract_rss_data(rss_data):
-    """Extract titles and descriptions from RSS feed data."""
-    soup = BeautifulSoup("", "html.parser")
-    for entry in rss_data.entries:
-        title = BeautifulSoup(entry.title, 'html.parser').get_text(strip=True)
-        description = BeautifulSoup(entry.description, 'html.parser').get_text(strip=True).replace('&gt', '')
-        wrapped_description = textwrap.fill(description, width=70)
-        soup.append(f"{title}\n\n{wrapped_description}\n\n")
-    return str(soup)
-
-def save_rss_data_as_txt(rss_data, txt_file_name):
-    """Save RSS feed data as a text file."""
-    with open(txt_file_name, 'w', encoding='utf-8') as f:
-        f.write(rss_data)
-    print(f"Created: {txt_file_name}")
-
-
-def txt_to_pdf(txt_file, pdf_file):
-    """Convert text from a .txt file to a PDF file."""
-    pdf = FPDF()
-    pdf.set_auto_page_break(auto=True, margin=15)
-    pdf.add_page()
-
-    # Set font for the PDF; make sure the font is available
-    try:
-        pdf.add_font('ArialUnicodeMS', '', 'ArialUnicodeMS.TTF', uni=True)
-        pdf.set_font('ArialUnicodeMS', '', 8)
-    except Exception as e:
-        print(f"Could not load font: {e}. Default font will be used.")
-        pdf.set_font('Arial', '', 8)  # Fallback to a default font if custom font fails
-
-    # Read the content of the text file and add it to the PDF
-    with open(txt_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            pdf.multi_cell(0, 2, line)
-
-    # Output the generated PDF to file
-    pdf.output(pdf_file)
-
-
 if __name__ == "__main__":
-    # RSS feed URLs
-    rss_urls = ["https://feeds.npr.org/510318/podcast.xml"]
-    
-    # Generate the file path for saving the RSS data
-    date_str = datetime.now().strftime('%Y-%m-%d')
-    file_path = f"RSS-Feed_{date_str}"
-    
-    # Fetch and save RSS feed data
-    rss_data = "".join(extract_rss_data(fetch_rss_feed(url)) for url in rss_urls)
-    txt_file_name = f"{file_path}.txt"
-    save_rss_data_as_txt(rss_data, txt_file_name)
-
-    pdf_file_name = f"{file_path}.pdf"
-    txt_to_pdf(txt_file_name, pdf_file_name)
-    
-    # Upload the PDF to Google Drive
-    upload_to_google_drive(pdf_file_name, FOLDER_ID)
+    for i, u in enumerate(FEED_URLS):
+        rss_to_ebook(u,EBOOK_NAMES[i])
+        # Upload the PDF to Google Drive
+        upload_to_google_drive(EBOOK_NAMES[i], FOLDER_ID)
